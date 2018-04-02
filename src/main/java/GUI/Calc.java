1f0acbf849f9077922484b44951fdc74d443da19
Jonathan Ivy <ivyjs@hendrix.edu>
2018-04-02 10:29:45 -0500

    Added JUnit to compile and fixed Mac not liking FXML

--- src/main/java/GUI/Calc.java
+++ src/main/java/GUI/Calc.java
@@ -2,6 +2,8 @@ package GUI;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.Border;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -10,7 +12,15 @@ public class Calc extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("CalcGUI.fxml"));
+			//if this is on a Mac
+			BorderPane root;
+
+			if(System.getProperty("os.name").startsWith("Mac")) {
+				 root = FXMLLoader.load(getClass().getResource("/CalcGUI.fxml"));
+			}else{
+				 root = FXMLLoader.load(getClass().getResource("CalcGUI.fxml"));
+
+			}
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.show();
