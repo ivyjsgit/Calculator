8010383123bd0c1ba988d1f51112d189622cf3cf
Colby-Hall <hallcc@hendrix.edu>
2018-04-01 23:14:24 -0500

    more testing

--- src/main/java/GUI/CalcController.java
+++ src/main/java/GUI/CalcController.java
@@ -4,8 +4,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
+import java.io.FileNotFoundException;
 import java.sql.*;
 
 import javax.script.ScriptException;
@@ -17,14 +19,14 @@ import DataStructures.InputContainers.InputAdders.*;
 
 public class CalcController {
 
-	// @FXML
-	// private ObservableList<String> calculations = FXCollections.observableArrayList();
+	@FXML
+	private ListView<String> info;
+
+	@FXML
+	private ObservableList<String> calculations = FXCollections.observableArrayList();
 
 	@FXML
 	private TextField input;
-
-	@FXML
-	private TextField test;
 
 	@FXML
 	private Button solver;
@@ -32,27 +34,28 @@ public class CalcController {
 	private String userEntered;
 
 	/*
-	HistoryDatabase history = new HistoryDatabase();
-	FunctionsDatabase functions = new FunctionsDatabase(history.getCon());
-	DatabaseContainer databases = new DatabaseContainer(history,functions);
-	*/
+	 * HistoryDatabase history = new HistoryDatabase(); FunctionsDatabase
+	 * functions = new FunctionsDatabase(history.getCon()); DatabaseContainer
+	 * databases = new DatabaseContainer(history, functions);
+	 */
+
+	public void initialize() {
+		info.setItems(calculations);
+	}
 
 	public void userInput() {
 		userEntered = input.getText();
 	}
 
-
-
 	public void calculate() throws ClassNotFoundException, SQLException, ScriptException {
 		userInput();
 		HistoryDatabase history = new HistoryDatabase();
 		FunctionsDatabase functions = new FunctionsDatabase(history.getCon());
-		DatabaseContainer databases = new DatabaseContainer(history,functions);
+		DatabaseContainer databases = new DatabaseContainer(history, functions);
 		CommandConverter converter = new CommandConverter(userEntered, databases);
 		String result = converter.run();
-		test.setText(result);
-		// calculations.add(result);
-
+		calculations.add(result);
+		input.clear();
 
 	}
 }
