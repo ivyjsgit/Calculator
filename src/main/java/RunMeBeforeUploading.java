d1379185635b8359a355685ee4fcb43b92745c95
JoshuaNorris <joshua.norris52@gmail.com>
2018-04-08 14:02:02 -0500

    addedFunctions

--- src/main/java/RunMeBeforeUploading.java
+++ src/main/java/RunMeBeforeUploading.java
@@ -15,6 +15,18 @@ public class RunMeBeforeUploading {
         ArrayList<String> functions = new ArrayList<>();
         functions.add("areasquare s = Math.pow( s , 2 )");
         functions.add("slope x1 x2 y1 y2 = (y2-y1)/(x2-x1)");
+        functions.add("quadraticPlus a b c = ((-b) + (Math.sqrt(Math.pow(b, 2) - 4 * a * c) ) / (2 * a)"); //
+        functions.add("quadraticMinus a b c = ((-b) - (Math.sqrt(Math.pow(b, 2) - 4 * a * c) ) / (2 * a)"); //
+        functions.add("pythogorenTheorem a b = Math.pow(a, 2) + Math.pow(b, 2)"); //
+        functions.add("areaOfRectangle a b = a * b");
+        functions.add("volumeOfRectangle a b c = a * b * c");
+        functions.add("areaOfTriangle a b = (a * b) / 2");
+        functions.add("areaOfCircle r = Math.PI * Math.pow(r, 2)");
+        functions.add("volumeOfSphere r = (Math.PI * (Math.pow(r, 3)) * (4/3)"); //
+        functions.add("volumeOfCylinder h r = (Math.PI * (Math.pow(r, 2)) * h"); //
+        functions.add("volumeOfCone r h = (Math.PI * Math.pow(r, 2) * h) / 3");
+        
+        
 
         for(String s:functions){
             databaseContainer.addFunction(s);
